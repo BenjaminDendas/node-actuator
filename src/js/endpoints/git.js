52bc51b7d9c7938c8f261e71713f3df38ba86878
Benjamin Dendas <benjamin.dendas@craftworkz.be>
2019-07-28 00:35:09 +0200

    Adding documentation for git endpoint

--- src/js/endpoints/git.js
+++ src/js/endpoints/git.js
@@ -1,28 +1,44 @@
-const readline = require('readline');
 const fs = require('fs');
 
+/**
+ * @description returns the last 5 actions on the repository
+ * @param {string} localFolderPath The path of the /.git/logs/refs/heads/ folder
+ * @param {string} branchname The name of the branch that we will monitor
+ * @returns {Array} returns an array with the 5 last local actions on the repository
+ */
 const getLocalGitStatusForBranch = (localFolderPath, branchname) => {
     return readGitInfo(localFolderPath, branchname);
 }
 
-
+/**
+ * @description reads the file
+ * @param {string} localFolderPath The path of the /.git/logs/refs/heads/ folder
+ * @param {string} branchname The name of the branch that we will monitor
+ * @returns {Array} returns an array with the 5 last local actions on the repository
+ */
 const readGitInfo = async (filePath, branchname) => {
     let content;
-
     return new Promise((resolve, reject) => {
         fs.readFile(filePath + branchname, 'utf-8', (err, data) => {
             if (err) {
-                console.log(err);
+                return reject(null);
             }
             let splittedData = data.split('\n');
             content = splittedData.slice(0, splittedData.length - 1)
+            if (content.length <= 5) {
+                return resolve(parseGitInfo(content));
+            }
+            content = content.slice(content.length - 5, content.length);
             return resolve(parseGitInfo(content));
         });
     });
-
-
 }
 
+/**
+ * @description parses the 5 lines of data to a list of objects
+ * @param {Array} info The path of the /.git/logs/refs/heads/ folder
+ * @returns {object} returns an array with the 5 last local actions on the repository converted to objects
+ */
 const parseGitInfo = (info) => {
     gitElements = [];
     info.forEach(element => {
@@ -37,7 +53,6 @@ const parseGitInfo = (info) => {
     });
     return gitElements;
 }
-
 
 module.exports = {
     getLocalGitStatusForBranch
